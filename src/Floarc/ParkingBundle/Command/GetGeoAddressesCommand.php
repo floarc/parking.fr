3b90b99ab495a92423fad538192f2a19b3e06277
floarc <florent.arcambal@gmail.com>
2014-02-17 01:10:32 +0100

    Entite et commande de console servant a remplir une table de geoloc

---
+++ src/Floarc/ParkingBundle/Command/GetGeoAddressesCommand.php
@@ -0,0 +1,272 @@
+<?php
+namespace Floarc\ParkingBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Doctrine\ORM\EntityManager;
+use Floarc\ParkingBundle\Entity\GeoAddress;
+use Floarc\ParkingBundle\Entity\Address;
+use Floarc\ParkingBundle\Entity\Parking;
+use Floarc\ParkingBundle\Entity\TypeContrat;
+use Floarc\ParkingBundle\Entity\TypeDuree;
+use Floarc\ParkingBundle\Entity\TypePlace;
+use Lsw\ApiCallerBundle\Call\HttpGetJson;
+use Lsw\ApiCallerBundle\Call\HttpGetHtml;
+use Lsw\ApiCallerBundle\Call\CurlCall;
+use Symfony\Component\DomCrawler\Crawler;
+
+class GetGeoAddressesCommand extends Command
+{
+	protected function configure()
+	{
+		$this
+		->setName('floarc:addad')
+		->setDescription('Recup d\'adresses geolocalise')
+		->addArgument(
+				'nb',
+				InputArgument::OPTIONAL,
+				'Combien d\'adresse souhaitez vous recup?'
+		)
+		/*
+		->addOption(
+				'yell',
+				null,
+				InputOption::VALUE_NONE,
+				'Si défini, la réponse est affichée en majuscules'
+		)
+		*/
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$this->input = $input;
+		$this->output = $output;
+		$this->nb = $input->getArgument('nb');
+		if ($this->nb) {
+			$text = 'Salut, '.$this->nb;
+			$this->output->writeln($text);
+			$this->output->writeln($this->nb." geoaddresses will be created !");
+			for ($i = 1; $i <= $this->nb; $i++) {
+				
+				
+				$this->createGeoAdress();
+				$this->output->writeln($i."/".$this->nb." geoaddresses created !");
+			}
+			$this->output->writeln($this->nb." geoaddresses created !");
+			
+		} else {
+			$this->output->writeln("1 geoaddress will be created !");
+			$this->createGeoAdress();
+			$this->output->writeln("1 geoaddress created !");
+		}
+
+		/*
+		if ($input->getOption('yell')) {
+			$text = strtoupper($text);
+		}
+		*/
+		
+		
+		
+		
+		
+		
+
+
+
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	protected function createGeoAdress()
+	{
+		$geoaddress = null;
+		$status = "";
+		$has_street_number = false;
+		
+		while ($status!="OK" || $has_street_number != true) {
+			sleep(10);
+			if($status=="OVER_QUERY_LIMIT"){
+				continue;
+			}
+			$this->output->writeln("");	
+			$this->output->writeln("");	
+			//Marseille
+			$lat_range = 57900;
+			$lat = (43306500-rand(0, $lat_range))/1000000;
+			$lon_range = 63500;
+			$lon = (5364400-(rand(0, $lon_range)))/1000000;
+		
+			$url_json = "http://maps.googleapis.com/maps/api/geocode/json?language=fr-FR&latlng=".$lat.",".$lon."&sensor=true";
+			$this->output->writeln($url_json);
+				
+			$this->container = $this->getApplication()->getKernel()->getContainer();
+				
+			//$this->output_json = $this->container->get('api_caller')->call(new HttpGetHtml($url_json, null ));
+			//$this->output_json = get_class($this->container->get('api_caller'));
+			$output_json = $this->container->get('api_caller')->call(new HttpGetHtml($url_json, null ));
+			$output_json_decoded = json_decode($output_json);
+				
+				
+			$status = $output_json_decoded->status;
+			//$this->output->writeln($status);
+			
+			//$this->output->writeln($output_json);
+			$results = $output_json_decoded->results;
+			$has_street_number = false;
+			$has_route = false;
+			foreach ($results as $key => $result) {
+				unset($street_number,$route,$postal_code,$locality,$departement,$country,$lat,$lon);
+				$address_components = $result->address_components;
+				//$address_components = $first_result->filter('address_component');
+				$geoaddress = new GeoAddress();
+				foreach ($address_components as $key => $address_component) {
+					$types=$address_component->types;
+					$type = $types[0];
+					switch ($type) {
+						case "street_number":
+							$street_number = $address_component->short_name;
+							if(!empty($street_number)){
+								$has_street_number = true;
+								echo "street_number =".$street_number."\n";
+								$geoaddress->setNumeroRue($street_number);								
+							}
+							break;
+						case "route":
+							$route = $address_component->long_name;
+							if(!empty($route)){
+								$has_route = true;
+								echo "route =".$route."\n";
+								$geoaddress->setRoute($route);							
+							}
+							break;
+								
+						case "neighborhood":
+							$neighborhood = $address_component->long_name;
+							echo "neighborhood =".$neighborhood."\n";
+							break;
+								
+						case "sublocality":
+							$sublocality = $address_component->long_name;
+							echo "sublocality =".$sublocality."\n";
+							break;
+								
+						case "locality":
+							$locality = $address_component->long_name;
+							if(!empty($locality)){
+								$geoaddress->setVille($locality);
+								echo "locality =".$locality."\n";							
+							}							
+
+							break;
+								
+						case "administrative_area_level_2":
+							$ad_area_lv2 = $address_component->long_name;
+							break;
+								
+						case "administrative_area_level_1":
+							$ad_area_lv1 = $address_component->long_name;
+							$region = $ad_area_lv1;
+							if(!empty($region)){
+								$geoaddress->setRegion($region);
+								echo "region =".$region."\n";							
+							}							
+
+							break;
+								
+						case "country":
+							$country = $address_component->short_name;
+							if(!empty($country)){
+								$geoaddress->setPays($country);
+								echo "country =".$country."\n";
+							}
+
+							break;
+								
+						case "postal_code":
+							$postal_code = $address_component->short_name;
+							if(!empty($postal_code)){
+								$geoaddress->setCodePostal($postal_code);
+								echo "postal_code =".$postal_code."\n";
+							}							
+
+							break;
+						default:
+							break;
+					}
+				}
+				
+				if($has_street_number==true  && $has_route==true) {
+				
+					$geometry = $result->geometry;
+					$location = $geometry->location;
+					
+					$lat = $location->lat;
+					$lon = $location->lng;
+					
+					if(isset($lat)){
+						$geoaddress->setLat($lat);
+					}
+					if(isset($lon)){
+						$geoaddress->setLon($lon);
+					}
+					
+					if(isset($lat) && !empty($lat) && isset($lon) && !empty($lon) ){
+						$geoaddress->setLocation($lat.",".$lon);
+					}
+					
+					$nowDateTime = new \DateTime("now", new \DateTimeZone("Europe/Paris"));
+					$geoaddress->setCreatedAt($nowDateTime);
+					$geoaddress->setModifiedAt($nowDateTime);
+					
+					
+					if(isset($street_number) && isset($route)){
+						$geoaddress->setAddress1($street_number." ".$route);
+						echo $street_number." ".$route."\n";
+							
+
+					}
+					$geoaddress->setJson($output_json);
+					
+					
+					
+					if($has_street_number==true  && $has_route==true) {
+						break;
+					}else{
+						unset($street_number,$route,$postal_code,$locality,$departement,$country,$lat,$lon);
+						$status = "";
+						$has_street_number = false;
+						$has_route = false;
+						continue;
+					}				
+					break 2;
+				}				
+			}
+		}
+		
+		$manager = $this->container->get("doctrine.orm.entity_manager");
+		if(isset($street_number) /*&& isset($route)*/){
+			$manager->persist($geoaddress);
+			$manager->flush();
+			echo "geoaddress saved...!\n";
+			return $geoaddress;
+		}
+		unset($street_number,$route,$postal_code,$locality,$departement,$country,$lat,$lon);		
+		
+
+		return $geoaddress;
+		
+
+	}
+	
+}
---
+++ src/Floarc/ParkingBundle/Entity/GeoAddress.php
@@ -0,0 +1,560 @@
+<?php
+
+namespace Floarc\ParkingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GeoAddress
+ *
+ * @ORM\Table(name="geoaddress")
+ * @ORM\Entity(repositoryClass="Floarc\ParkingBundle\Entity\GeoAddressRepository")
+ */
+class GeoAddress
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var text
+     *
+     * @ORM\Column(name="json", type="text", nullable=true)
+     */
+    private $json;
+        
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address1", type="string", length=225, nullable=true)
+     */
+    private $address1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address2", type="string", length=255, nullable=true)
+     */
+    private $address2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address3", type="string", length=255, nullable=true)
+     */
+    private $address3;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_rue", type="string", length=50, nullable=true)
+     */
+    private $numeroRue;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="route", type="string", length=255, nullable=true)
+     */
+    private $route;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code_postal", type="string", length=5, nullable=true)
+     */
+    private $codePostal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=255, nullable=true)
+     */
+    private $ville;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="departement", type="string", length=255, nullable=true)
+     */
+    private $departement;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="region", type="string", length=255, nullable=true)
+     */
+    private $region;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pays", type="string", length=255, nullable=true)
+     */
+    private $pays;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lat", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $lat;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lon", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $lon;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255, nullable=true)
+     */
+    private $location;    
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="alt", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $alt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified_at", type="datetime", nullable=true)
+     */
+    private $modifiedAt;
+
+    /**
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+    	return "address = ".$this->id;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set address1
+     *
+     * @param string $json
+     * @return GeoAddress
+     */
+    public function setJson($json)
+    {
+    	$this->json = $json;
+    
+    	return $this;
+    }
+    
+    /**
+     * Set address1
+     *
+     * @param string $address1
+     * @return GeoAddress
+     */
+    public function setAddress1($address1)
+    {
+        $this->address1 = $address1;
+
+        return $this;
+    }
+
+    /**
+     * Get json
+     *
+     * @return string
+     */
+    public function getJson()
+    {
+    	return $this->json;
+    }    
+    
+    /**
+     * Get address1
+     *
+     * @return string 
+     */
+    public function getAddress1()
+    {
+        return $this->address1;
+    }
+
+    /**
+     * Set address2
+     *
+     * @param string $address2
+     * @return GeoAddress
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    /**
+     * Get address2
+     *
+     * @return string 
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * Set address3
+     *
+     * @param string $address3
+     * @return GeoAddress
+     */
+    public function setAddress3($address3)
+    {
+        $this->address3 = $address3;
+
+        return $this;
+    }
+
+    /**
+     * Get address3
+     *
+     * @return string 
+     */
+    public function getAddress3()
+    {
+        return $this->address3;
+    }
+
+    /**
+     * Set numeroRue
+     *
+     * @param string $numeroRue
+     * @return GeoAddress
+     */
+    public function setNumeroRue($numeroRue)
+    {
+        $this->numeroRue = $numeroRue;
+
+        return $this;
+    }
+
+    /**
+     * Get numeroRue
+     *
+     * @return string 
+     */
+    public function getNumeroRue()
+    {
+        return $this->numeroRue;
+    }
+
+    /**
+     * Set route
+     *
+     * @param string $route
+     * @return GeoAddress
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Get route
+     *
+     * @return string 
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param string $codePostal
+     * @return GeoAddress
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return string 
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     * @return GeoAddress
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string 
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set departement
+     *
+     * @param string $departement
+     * @return GeoAddress
+     */
+    public function setDepartement($departement)
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    /**
+     * Get departement
+     *
+     * @return string 
+     */
+    public function getDepartement()
+    {
+        return $this->departement;
+    }
+
+    /**
+     * Set region
+     *
+     * @param string $region
+     * @return GeoAddress
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * Get region
+     *
+     * @return string 
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * Set pays
+     *
+     * @param string $pays
+     * @return GeoAddress
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    /**
+     * Get pays
+     *
+     * @return string 
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param float $lat
+     * @return GeoAddress
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return float 
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * Set lon
+     *
+     * @param float $lon
+     * @return GeoAddress
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    /**
+     * Get lon
+     *
+     * @return float 
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+    
+    /**
+     * Set location
+     *
+     * @param string $location
+     * @return Parking
+     */
+    public function setLocation($location)
+    {
+    	$this->location = "";
+    	if(!empty($location)){
+    		$this->location = $location;
+    	}else{
+    		$lat = $this->getLat();
+    		$lon = $this->getLon();
+    		if(!empty($lat) && !empty($lon)){
+    			$this->location = $lat.",".$lon;
+    		}
+    	}
+    	return $this;
+    }    
+    
+    /**
+     * Return the location
+     *
+     * @return string
+     */
+    public function getLocation()
+    {
+    	return $this->location;
+    }    
+
+    /**
+     * Set alt
+     *
+     * @param float $alt
+     * @return GeoAddress
+     */
+    public function setAlt($alt)
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    /**
+     * Get alt
+     *
+     * @return float 
+     */
+    public function getAlt()
+    {
+        return $this->alt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return GeoAddress
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set modifiedAt
+     *
+     * @param \DateTime $modifiedAt
+     * @return GeoAddress
+     */
+    public function setModifiedAt($modifiedAt)
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get modifiedAt
+     *
+     * @return \DateTime 
+     */
+    public function getModifiedAt()
+    {
+        return $this->modifiedAt;
+    }
+}
